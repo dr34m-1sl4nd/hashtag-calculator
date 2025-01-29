43e70305a7c9606e32986264a2e52d2bcbec26d7
ROYGBIV <royston.phetmyxay24@student.cart.org>
2025-01-29 13:40:31 -0800

    build calculator stupid

---
+++ main.js
@@ -0,0 +1,7 @@
+let num1 = prompt(`What is your first number?`);
+
+if (num1 != NaN) {
+    document.getElementById(`input`).innerHTML = `Your first number is` + num1;
+}
+
+let num2 = prompt(`What is your second number?`);
