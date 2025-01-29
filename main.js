f1a8bfdb5c915aba620c02c4a8b26e25ac65efb4
ROYGBIV <royston.phetmyxay24@student.cart.org>
2025-01-29 14:22:10 -0800

    figure it out

--- main.js
+++
@@ -1,7 +0,0 @@
-let num1 = prompt(`What is your first number?`);
-
-if (num1 != NaN) {
-    document.getElementById(`input`).innerHTML = `Your first number is` + num1;
-}
-
-let num2 = prompt(`What is your second number?`);
